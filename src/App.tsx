0da36a90ed2a0863975718f0c8e5efdbdc3a6c7c
santiagato <est.smendozal0201@uml.edu.ni>
2023-08-12 16:00:25 -0600

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Hotels } from "./pages/Hotels";
 import { Flights } from "./pages/Flights";
 import { Foods } from "./pages/Foods";
 import { Transports } from "./pages/Transports";
+import { Registros } from "./pages/Registros";
 
 function App() {
   return (
@@ -28,6 +29,9 @@ function App() {
             <Route
               path="/transports"
               element={<Transports />}></Route>
+               <Route
+              path="/registros"
+              element={<Registros />}></Route>
           
           </Routes>
           </div>
